e5d3f6d55b0cc13a4309fab10c085560a8582844
andicoaker <Andi@Andreas-MacBook.local>
2017-02-08 16:51:18 -0500

    built basic html layout, including bootstrap table for home page

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,37 @@
-console.log('wat')
+import $ from 'jquery';
+
+$.getJSON('http://apis.is/concerts').then(function(serverRes){
+console.log(serverRes);
+
+  var htmlTemplate = createArticlePageTemplate( serverRes.results, "Concerts" )
+  appContainerEl.innerHTML = htmlTemplate
+
+})
+
+$.getJSON('http://apis.is/rides/samferda-drivers/').then(function(serverRes){
+  console.log(serverRes);
+
+  var htmlTemplate = createArticlePageTemplate( serverRes.results, "Carpools" )
+  appContainerEl.innerHTML = htmlTemplate
+
+
+
+})
+
+$.getJSON('http://apis.is/flight?language=en&type=departures').then(function(serverRes){
+  console.log(serverRes);
+
+  var htmlTemplate = createArticlePageTemplate( serverRes.results, "Flights" )
+  appContainerEl.innerHTML = htmlTemplate
+
+
+})
+
+$.getJSON('http://apis.is/flight?language=en&type=arrivals').then(function(serverRes){
+  console.log(serverRes);
+
+  var htmlTemplate = createArticlePageTemplate( serverRes.results, "Technology" )
+  appContainerEl.innerHTML = htmlTemplate
+
+
+})
